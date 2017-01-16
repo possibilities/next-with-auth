6930365dc1fcba28df348db768656fae0b51ba9b
Mike Bannister <notimpossiblemike@gmail.com>
2017-01-15 19:10:41 -0800

    Dedupe some styles

--- components/Menu.js
+++ components/Menu.js
@@ -3,53 +3,36 @@ import Link from 'next/link'
 export default ({ username }) => {
   return (
     <div>
-      <ul>
-        <style jsx>{`
-          ul {
-            padding: 0 0 20px;
-            margin: 0;
-          }
-          li {
-            display: inline;
-            list-style: none;
-          }
-          li:after {
-            content: " | ";
-          }
-          li:last-child:after {
-            content: "";
-          }
-        `}</style>
+      <style jsx>{`
+        ul {
+          padding: 0 0 20px;
+          margin: 0;
+        }
+        ul.pull-right {
+          padding: 0;
+          position: absolute;
+          right: 0;
+          top: 0;
+        }
+        li {
+          display: inline;
+          list-style: none;
+        }
+        li:after {
+          content: " | ";
+        }
+        li:last-child:after {
+          content: "";
+        }
+      `}</style>
 
+      <ul>
         <li><Link href='/'>home</Link></li>
         <li><Link href='/other'>other</Link></li>
         <li><Link href='/secret'>secret</Link></li>
       </ul>
 
-      <ul>
-        <style jsx>{`
-          ul {
-            padding: 0;
-            margin: 0;
-            position: absolute;
-            right: 0;
-            top: 0;
-          }
-          li {
-            display: inline;
-            list-style: none;
-          }
-          li:after {
-            content: " | ";
-          }
-          li:last-child:after {
-            content: "";
-          }
-          li span {
-            color: #aaa;
-          }
-        `}</style>
-
+      <ul className='pull-right'>
         {!username && <li><Link href='/sign-in'>sign in</Link></li>}
         {!username && <li><Link href='/sign-up'>sign up</Link></li>}
         {username && (
